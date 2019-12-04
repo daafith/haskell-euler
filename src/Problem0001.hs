a24f4e8de4bb8a2536ae81bc587de6ab803492a7
daafith <davidbaak@gmail.com>
2019-12-04 20:08:28 +0100

    pretty print

--- src/Problem0001.hs
+++ src/Problem0001.hs
@@ -6,7 +6,13 @@ module Problem0001
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
 main :: IO ()
-main = print (sum (multiplesOfThreeOrFive 999))
+main = do
+        putStrLn description
+        putStrLn "The answer is:"
+        print (sum (multiplesOfThreeOrFive 999))
+
+description = "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.\
+\ The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.\n"
 
 multiplesOfThreeOrFive :: Integral a => a -> [a]
 multiplesOfThreeOrFive upperbound = [x | x <- [1..upperbound], x `isMultipleOf` [3,5]]
