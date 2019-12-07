6a646939cddf9cf202850315eef914284aae07b3
David Baak <davidbaak@gmail.com>
2019-12-07 13:56:13 +0100

    pretty print

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,5 +7,10 @@ import qualified Problem0002
 
 runner :: IO ()
 runner = do
+        separator
         Problem0001.main
+        separator
         Problem0002.main
+        separator
+
+separator = putStrLn "\n|------------------|"
