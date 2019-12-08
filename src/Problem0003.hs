7ffc63d68fa7569b71093b4e2c34ff173d933ab2
David Baak <davidbaak@gmail.com>
2019-12-08 19:51:46 +0100

    ok, first in ugly java

--- src/Problem0003.hs
+++ src/Problem0003.hs
@@ -15,6 +15,21 @@ main = do
 description = "\nProblem 3:\nThe prime factors of 13195 are 5, 7, 13 and 29.\
 \What is the largest prime factor of the number 600851475143 ?\n"
 
+-- solved it in simple, ugly Java , now it's time for haskell
+--  public static void main(String[] args) {
+--   long n = 600851475143L;
+--     ArrayList<Integer> pfs = new ArrayList<>();
+
+--     for (int p = 2; p<=n; p++) {
+--       if (n % p == 0) {
+--         pfs.add(p);
+--         n = n / p;
+--       }
+--     }
+--     pfs.forEach(pf -> System.out.println(pf));
+--     System.out.println("The highest prime factor is " + Collections.max(pfs)); // 6857
+--   }
+
 
 
 
