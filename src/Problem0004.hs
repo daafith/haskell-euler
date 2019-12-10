1e43ba24dc50e43c95a1b56d174123e89e7deab8
daafith <davidbaak@gmail.com>
2019-12-10 16:04:52 +0100

    improved performance a bit

--- src/Problem0004.hs
+++ src/Problem0004.hs
@@ -10,13 +10,22 @@ main :: IO ()
 main = do
     putStrLn description
     putStrLn "The answer is:"
-    let answer = last $ palindromes 999
+    let answer = maximum $ palindromes 100 999
     print answer
 
 description = "\nA palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.\
 \Find the largest palindrome made from the product of two 3-digit numbers.\n"
 
-palindromes n = sortUniq [ x*y | x <- [1..n] , y <- [1..n], isPalindrome (x*y)]
+palindromes l u = [ x*y::Int | x <- [u, u-1..l] , y <- [x, x-1..l], isPalindrome (x*y) ]
     where isPalindrome p =  (read . reverse . show $ p) == p
 
+
+-- is a quicker version that I found somewhere else but need to research Control.Applicative
+-- http://hackage.haskell.org/package/base-4.12.0.0/docs/Control-Applicative.html
+maxPalindrome = print $ maximum
+    [ x * y :: Int
+    | x <- [999, 998 .. 100]
+    , y <- [x, x-1 .. 100]
+    , (==) <*> reverse $ show $ x * y
+    ]
 
